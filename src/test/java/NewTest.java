6abb39a89faad2ce0b1893c1a382c5724398416a
Vahe1982 <vahe1982@yandex.ru>
2016-04-12 17:17:55 +0400

    commit10

--- src/test/java/NewTest.java
+++ src/test/java/NewTest.java
@@ -1,8 +1,15 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
+
 /**
  * Created by PC1 on 12.04.2016.
  */
 public class NewTest {
-    public static void main(String[] args) {
+    @org.testng.annotations.Test
+    public void testing() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.youtube.com/");
         System.out.println("Jenkins");
     }
 }
