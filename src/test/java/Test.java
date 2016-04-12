e8d8c4a2e843fa80da8ff5283780d2db324ea605
Vahe1982 <vahe1982@yandex.ru>
2016-04-12 10:16:51 +0400

    first commit

---
+++ src/test/java/Test.java
@@ -0,0 +1,27 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by PC1 on 06.04.2016.
+ */
+public class Test {
+   @org.testng.annotations.Test
+    //public void testingWebElements() throws InterruptedException {
+   public static void main() throws Exception {
+
+
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://www.youtube.com/");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+        driver.findElement(By.xpath(".//*/li[1]/div/div[1]/div[1]/a/div/span/img")).click();
+        driver.findElement(By.xpath(".//*[@id='movie_player']/div[1]/video")).click();
+        driver.findElement(By.xpath(".//*[@id='movie_player']/div[1]/video")).click();
+        System.out.println("OK");
+
+    }
+}
